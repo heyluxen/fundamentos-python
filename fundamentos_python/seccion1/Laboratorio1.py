5d970407c22200ad004d37b25e6d64637827160c
Valentina <valentinacorrea0117@gmail.com>
2026-04-21 20:18:14 -0500

    correccion

--- fundamentos_python/seccion1/Laboratorio1.py
+++ fundamentos_python/seccion1/Laboratorio1.py
@@ -1,2 +1,10 @@
 print("¡Hola, Mundo!")
 print("Valentina")
+
+#print(¡Hola, Mundo!)
+#print "¡Hola, Mundo!"
+#print('¡Hola, Mundo!')
+#print('Valentina')
+#print("Hola"); print("Mundo")
+#print("¡Hola, Mundo!\nValentina")
+#Print("Hola")
